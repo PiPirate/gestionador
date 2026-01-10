045a9dda23f7d927ed13bb5addd7028cfccf6347
PiPirate <134560763+PiPirate@users.noreply.github.com>
2026-01-10 11:26:28 -0500

    Update investor time invested to use current date

--- routes/modules/investors/web.php
+++ routes/modules/investors/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Modules\InvestorsController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/investors', [InvestorsController::class, 'index'])->name('investors.index');
+    Route::get('/investors/{investor}', [InvestorsController::class, 'show'])->name('investors.show');
     Route::post('/investors', [InvestorsController::class, 'store'])->name('investors.store');
     Route::put('/investors/{investor}', [InvestorsController::class, 'update'])->name('investors.update');
     Route::delete('/investors/{investor}', [InvestorsController::class, 'destroy'])->name('investors.destroy');
